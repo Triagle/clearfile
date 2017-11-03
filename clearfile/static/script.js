0076550cfb9f61defc01dbae42eb68cd8c72b2ed
Jake Faulkner <jakefaulkn@gmail.com>
2017-11-03 16:20:40 +1300

    Convert to materialize.css

--- clearfile/static/script.js
+++ clearfile/static/script.js
@@ -1,18 +1,42 @@
+function buildDom(nodes) {
+    for (let index = 1; index < nodes.length; ++index) {
+        let child = nodes[index];
+        if (child.constructor === Array) {
+            child = buildDom(child);
+        }
+        root.appendChild(child);
+    }
+    return root;
+}
+
 function addResult(event) {
     if (event.keyCode == 13) {
         var clearfile_search_query = document.getElementById("clearfile-search-input");
         var clearfile_search_results = document.getElementById("clearfile-search-results");
-        var result = document.createElement("li");
-        result.classList.add("clearfile-search-result", "base07-background");
-        var content = document.createElement("div");
-        content.classList.add("clearfile-search-content");
-        var title = document.createElement("b");
-        title.appendChild(document.createTextNode("Search Query"));
+        var row = document.createElement("div");
+        row.classList.add("row");
+        var card = document.createElement("div");
+        card.classList.add("card");
+        var card_image = document.createElement("div");
+        card_image.classList.add("card-image");
+        var image = document.createElement("img");
+        image.setAttribute("src", "https://athlonecommunityradio.ie/wp-content/uploads/2017/04/placeholder.png");
+        var card_title = document.createElement("span");
+        card_title.classList.add("card-title");
+        card_title.appendChild(document.createTextNode("Search Result"));
+        card_content = document.createElement("div");
+        card_content.classList.add("card-content");
         var description = document.createElement("p");
         description.appendChild(document.createTextNode(clearfile_search_query.value));
-        content.appendChild(title);
-        content.appendChild(description);
-        result.appendChild(content);
-        clearfile_search_results.append(result);
+
+        card = buildDom([row,
+                  [card,
+                   [card_image,
+                    image,
+                    card_title],
+                   [card_content,
+                    description]]]);
+
+        clearfile_search_results.append(card);
     }
 }
