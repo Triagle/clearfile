69b88b3df346cfd238dae0b94dc5312787de04ae
Jake Faulkner <jakefaulkn@gmail.com>
2017-11-07 15:58:45 +1300

    Write some functions to create results as cards not list items

--- clearfile/static/script.js
+++ clearfile/static/script.js
@@ -1,4 +1,5 @@
 function buildDom(nodes) {
+    let root = nodes[0];
     for (let index = 1; index < nodes.length; ++index) {
         let child = nodes[index];
         if (child.constructor === Array) {
@@ -9,34 +10,47 @@ function buildDom(nodes) {
     return root;
 }
 
+function buildNoteCard(title, image_link, description, link) {
+    var row = document.createElement("div");
+    row.classList.add("row");
+    var card = document.createElement("div");
+    card.classList.add("card");
+    var card_image = document.createElement("div");
+    card_image.classList.add("card-image");
+    var image = document.createElement("img");
+    image.src = image_link;
+    // image.setAttribute("src", image);
+    var card_title = document.createElement("span");
+    card_title.classList.add("card-title");
+    card_title.appendChild(document.createTextNode(title));
+    card_content = document.createElement("div");
+    card_content.classList.add("card-content");
+    var description_el = document.createElement("p");
+    description_el.appendChild(document.createTextNode(description));
+    var card_action = document.createElement("div");
+    card_action.classList.add("card-action");
+    var note_link = document.createElement("a");
+    note_link.href = link;
+    note_link.innerHTML = "Download";
+
+
+    var card = buildDom([row,
+                     [card,
+                      [card_image,
+                       image,
+                       card_title],
+                      [card_content,
+                       description_el],
+                      [card_action,
+                       note_link]]]);
+   return card
+}
+
 function addResult(event) {
     if (event.keyCode == 13) {
         var clearfile_search_query = document.getElementById("clearfile-search-input");
         var clearfile_search_results = document.getElementById("clearfile-search-results");
-        var row = document.createElement("div");
-        row.classList.add("row");
-        var card = document.createElement("div");
-        card.classList.add("card");
-        var card_image = document.createElement("div");
-        card_image.classList.add("card-image");
-        var image = document.createElement("img");
-        image.setAttribute("src", "https://athlonecommunityradio.ie/wp-content/uploads/2017/04/placeholder.png");
-        var card_title = document.createElement("span");
-        card_title.classList.add("card-title");
-        card_title.appendChild(document.createTextNode("Search Result"));
-        card_content = document.createElement("div");
-        card_content.classList.add("card-content");
-        var description = document.createElement("p");
-        description.appendChild(document.createTextNode(clearfile_search_query.value));
-
-        card = buildDom([row,
-                  [card,
-                   [card_image,
-                    image,
-                    card_title],
-                   [card_content,
-                    description]]]);
-
-        clearfile_search_results.append(card);
+        var card = buildNoteCard(clearfile_search_query.value, "http://via.placeholder.com/300", "Some note of some description", "http://via.placeholder.com/300");
+        clearfile_search_results.appendChild(card);
     }
 }
